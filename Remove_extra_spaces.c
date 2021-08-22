2509956c988ee66bc1d4519047d8e101db7df8fd
Priyanuj Bora <priyanuj1902assam@gmail.com>
2021-08-22 23:27:46 +0530

    Removin extra spaces from a string

---
+++ Remove_extra_spaces.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void remove_spaces(char *, int);
+int main(void)
+{
+    char *s;
+
+    int count=0;
+
+    s=(char *)malloc(120);
+
+    printf("enter a string....");
+    gets(s);
+
+    for(int i=0; s[i]!='\0'; i++)
+    {
+        count++;
+    }
+
+    remove_spaces(s, count);
+
+    return 0;
+}
+
+
+void remove_spaces(char *s, int l)
+{
+    int i, j, space_count=0;
+
+    i=0;
+
+    while(s[i]!='\0')
+    {
+        if(s[i]==' ')
+        {
+            space_count++;
+            if(space_count>=2)
+            {
+                j=i;
+                while(j<l-1)
+                {
+                    s[j]=s[j+1];
+                    j++;
+                }
+
+                s[l-1]='\0';
+                l-=1;
+                i=0;
+                space_count=0;
+            }
+
+            if(s[i+1]!=' ')
+            {
+                space_count=0;
+            }
+
+            else
+            {
+                ;
+            }
+        }
+
+        i++;
+    }
+
+    printf("\n\nThe string after removing the extra spaces is....\n\n");
+    puts(s);
+    
+}
